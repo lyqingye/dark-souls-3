5ee2f11ed8df437f561030273728fa6a4dd77260
lyqingye <1316515719@qq.com>
2021-12-30 09:50:15 +0800

    add component

---
+++ dll/src/lib.rs
@@ -0,0 +1,29 @@
+#![cfg(windows)]
+
+use winapi::shared::minwindef;
+use winapi::shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID};
+use winapi::um::consoleapi;
+
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+extern "system" fn DllMain(
+    dll_module: HINSTANCE,
+    call_reason: DWORD,
+    reserved: LPVOID)
+    -> BOOL
+{
+    const DLL_PROCESS_ATTACH: DWORD = 1;
+    const DLL_PROCESS_DETACH: DWORD = 0;
+
+    match call_reason {
+        DLL_PROCESS_ATTACH => main(),
+        DLL_PROCESS_DETACH => (),
+        _ => ()
+    }
+    minwindef::TRUE
+}
+
+fn main() {
+    unsafe { consoleapi::AllocConsole() };
+    println!("Hello, world!");
+}
